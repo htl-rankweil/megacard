6c693217252e189dffc428ba6ac986d5f1d95dab
0x007E <raffael@sunriax.at>
2023-05-09 21:20:09 +0200

    Fix wrong identifier

--- library/uart/uart.c
+++ library/uart/uart.c
@@ -119,7 +119,7 @@ UART_Error uart_error_flags(void)
         return UART_Overrun; // Return NUL
     }
     // UART_Parity error
-    else if(UCSRA & (1<<PE))
+    else if(UCSRA & (1<<UPE))
     {
         UDR;           // Clear UART data register
         return UART_Parity;  // Return NUL
